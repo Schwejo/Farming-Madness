0be74ccf6cac04cd97f5ba9904844e4effe373c9
Jonathan Schweizer <jonathanschweizer1997@gmail.com>
2019-04-05 17:35:01 +0200

    Added male character sprites + animations, added some test ground sprites, added basic movement script

---
+++ Farming-Madness/Assets/Scripts/Character/BasicMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public Animator animator;
+
+    private Vector3 movement;
+    private Vector3 lastMovement;
+
+    public float speedMultiplier = 5.0f;
+
+
+    private void Update()
+    {
+        ProcessInputs();
+        Animate();
+        Move();
+    }
+
+    private void ProcessInputs()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        //Store last movement vector while walking.
+        if(movement.magnitude != 0)
+        {
+            lastMovement = movement;
+        }
+    }
+
+    private void Animate()
+    {
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        
+        animator.SetFloat("Magnitude", movement.magnitude);
+
+        animator.SetFloat("HorizontalIdle", lastMovement.x);
+        animator.SetFloat("VerticalIdle", lastMovement.y);
+    }
+
+    private void Move() 
+    {
+        movement = movement * speedMultiplier;
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+}
