eedff591b14e7991b60c4b48156bb1a27dde285e
Jonathan Schweizer <jonathanschweizer1997@gmail.com>
2019-05-30 15:17:12 +0200

    Created simple random movement script for animals

---
+++ Farming-Madness/Assets/Scripts/AnimalCage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalCage : MonoBehaviour
+{
+    public GameObject top;
+    public GameObject bottom;
+    public GameObject right;
+    public GameObject left;
+
+    public float GetTop()
+    {
+        return top.transform.position.y;
+    }
+
+    public float GetBottom()
+    {
+        return bottom.transform.position.y;
+    }
+
+    public float GetLeft()
+    {
+        return left.transform.position.x;
+    }
+
+    public float GetRight()
+    {
+        return right.transform.position.x;
+    }
+}
